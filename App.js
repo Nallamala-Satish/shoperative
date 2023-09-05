6e70d5ebf6ac364e92b8e1706f9f59514a1275a6
apple <apple@apples-MacBook-Pro.local>
2023-09-05 16:02:53 +0530

    ios setup

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, {useState, useEffect} from 'react';
-import {StatusBar} from 'react-native'
+import {SafeAreaView, StatusBar} from 'react-native'
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import { getUserProfileInfo } from './src/utils/AsyncStorageHelper';
@@ -25,12 +25,15 @@ const App = () => {
       checkUser()
     },[])
     return (
+    <SafeAreaView style={{flex:1, backgroundColor:'#ED7421'}}>
+
       <SafeAreaProvider>
         <StatusBar backgroundColor={'#ED7421'}/>
     
            <Routes/>
     
       </SafeAreaProvider>
+      </SafeAreaView>
     );
   };
 
